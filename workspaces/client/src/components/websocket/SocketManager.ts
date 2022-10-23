b29a00405012c03f1d9eaa75a319a65a65614654
Brian <bbbrianboylen@gmail.com>
2022-10-22 19:28:35 -0700

    add client socket manager class

---
+++ workspaces/client/src/components/websocket/SocketManager.ts
@@ -0,0 +1,140 @@
+import { io, Socket } from "socket.io-client";
+import { SetterOrUpdater } from "recoil";
+import { showNotification } from "@mantine/notifications";
+import { ClientEvents } from "@piclash/shared/client/ClientEvents";
+import { ServerEvents } from "@piclash/shared/server/ServerEvents";
+import { ServerExceptionResponse } from "@piclash/shared/server/types";
+import { Listener } from "@components/websocket/types";
+import { SocketState } from "@components/websocket/SocketState";
+
+type EmitOptions<T> = {
+  event: ClientEvents;
+  data?: T;
+};
+
+export default class SocketManager {
+  public readonly socket: Socket;
+
+  public setSocketState: SetterOrUpdater<SocketState>;
+
+  private connectionLost: boolean = false;
+
+  constructor() {
+    this.socket = io(process.env.NEXT_PUBLIC_WS_API_URL as string, {
+      autoConnect: false,
+      path: "/wsapi",
+      transports: ["websocket"],
+      withCredentials: true,
+    });
+
+    this.onConnect();
+    this.onDisconnect();
+    this.onException();
+  }
+
+  emit<T>(options: EmitOptions<T>): this {
+    this.socket.emit(options.event, options.data);
+    return this;
+  }
+
+  getSocketId(): string | null {
+    if (!this.socket.connected) return null;
+
+    return this.socket.id;
+  }
+
+  connect(): void {
+    this.socket.connect();
+  }
+
+  disconnect(): void {
+    this.socket.disconnect();
+  }
+
+  registerListener<T>(event: ServerEvents, listener: Listener<T>): this {
+    this.socket.on(event, listener);
+
+    return this;
+  }
+
+  removeListener<T>(event: ServerEvents, listener: Listener<T>): this {
+    this.socket.off(event, listener);
+
+    return this;
+  }
+
+  private onConnect(): void {
+    this.socket.on("connect", () => {
+      if (this.connectionLost) {
+        showNotification({
+          message: "Reconnected to server!",
+          color: "green",
+          autoClose: 2000,
+        });
+        this.connectionLost = false;
+      }
+    });
+  }
+
+  private onDisconnect(): void {
+    this.socket.on("disconnect", async (reason: Socket.DisconnectReason) => {
+      if (reason === "io client disconnect") {
+        showNotification({
+          message: "Disconnected successfully",
+          color: "green",
+          autoClose: 2000,
+        });
+      } else if (reason === "io server disconnect") {
+        showNotification({
+          message: "Disconnected from server",
+          color: "orange",
+          autoClose: 4000,
+        });
+      } else if (
+        reason === "ping timeout" ||
+        reason === "transport close" ||
+        reason === "transport error"
+      ) {
+        showNotification({
+          message: "Connection lost to the server",
+          color: "orange",
+          autoClose: 3000,
+        });
+        this.connectionLost = true;
+      }
+
+      // what should this type be?
+      this.setSocketState((currVal: any) => {
+        return { ...currVal, connected: false };
+      });
+    });
+  }
+
+  private onException(): void {
+    this.socket.on("exception", (data: ServerExceptionResponse) => {
+      if (typeof data.exception === "undefined") {
+        showNotification({
+          message: "Unexpected error from server",
+          color: "red",
+        });
+
+        return;
+      }
+
+      let body = `Error: ${data.exception}`;
+
+      if (data.message) {
+        if (typeof data.message === "string") {
+          body += ` | Message: "${data.message}"`;
+        } else if (typeof data.message === "object") {
+          body += ` | Message: "${JSON.stringify(data.message)}"`;
+        }
+      }
+
+      showNotification({
+        message: body,
+        color: "red",
+      });
+    });
+  }
+}
